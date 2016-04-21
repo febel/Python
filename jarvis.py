94294bd77a924daa7c3c57890ee8d0f696f6a11a
franck ebel <franck.ebel@serval-concept.com>
2016-04-21 08:44:46 +0200

    Create jarvis.py

---
+++ jarvis.py
@@ -0,0 +1,30 @@
+#code source trouvé ici : https://gist.githubusercontent.com/GGulati/1ebaeaaa7f7408647fef/raw/f8fc12d3a48a1cd6cee4fa1f1111335ef4dad350/Jarvis.py
+import speech_recognition
+import pyttsx
+
+speech_engine = pyttsx.init('sapi5') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
+speech_engine.setProperty('rate', 150)
+
+def speak(text):
+	speech_engine.say(text)
+	speech_engine.runAndWait()
+
+recognizer = speech_recognition.Recognizer()
+
+def listen():
+	with speech_recognition.Microphone() as source:
+		recognizer.adjust_for_ambient_noise(source)
+		audio = recognizer.listen(source)
+
+	try:
+		return recognizer.recognize_sphinx(audio)
+		# or: return recognizer.recognize_google(audio)
+	except speech_recognition.UnknownValueError:
+		print("Could not understand audio")
+	except speech_recognition.RequestError as e:
+		print("Recog Error; {0}".format(e))
+
+	return ""
+
+speak("Say something!")
+speak("I heard you say " + listen())
